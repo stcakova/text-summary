d4d68ae81b679a7de2e3fb215789649e53149e7c
stcakova <s_tzakova@abv.bg>
2017-01-26 17:59:39 +0200

    Update url of tomcat server the search is being sent to

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,12 +10,13 @@
 angular.module('textSummaryApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.search = ""
-    $scope.naRaliUrla = ""
 
 	$scope.onSubmit = function() {
+		var search = $scope.search.split(' ').join('+');
+    	var naRaliUrla = "http://localhost:8080/find-summarized-article?searchText=" + search;
    		$scope.summary = "What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 
-    	$http.post($scope.naRaliUrla, {search: $scope.search})
+    	$http.get(naRaliUrla)
     	.then(function(response) {
     		$scope.summary = response.summary;
     	}, function(error) {
